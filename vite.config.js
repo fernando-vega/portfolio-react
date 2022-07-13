5789ffdcda5a46a71a79119cc751fcc0c7df2bd4
Fernando Vega <juanfe.vega@gmail.com>
2022-07-12 22:56:56 -0500

    Change base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://fernando-vega.github.io/',
+  base: 'https://fernando-vega.github.io/portfolio-react/',
   plugins: [react()]
 })
