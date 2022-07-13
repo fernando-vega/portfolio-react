08a3185875e65435eda7004106cdb46fa25c7160
Fernando Vega <juanfe.vega@gmail.com>
2022-07-13 00:07:25 -0500

    Add more actions

---
+++ src/Link.test.js
@@ -0,0 +1,31 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable prettier/prettier */
+import renderer from 'react-test-renderer'
+import React from 'react';
+import Link from './Link'
+
+it('changes the class when hovered', () => {
+  const component = renderer.create(
+    <Link page="http://www.facebook.com">Facebook</Link>,
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseEnter()
+  })
+  // re-rendering
+  tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseLeave()
+  })
+  // re-rendering
+  tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
